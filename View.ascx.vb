f093a3e8e9aa44571111311c9107d5b83941def8
peppertree <becker@peppertree-solutions.com>
2014-07-22 11:17:31 +0200

    Interface specification should be Class, Assembly
    https://github.com/DNN-Connect/UserAccountRegistration/issues/9

--- View.ascx.vb
+++ View.ascx.vb
@@ -766,8 +766,8 @@ Namespace Connect.Modules.UserManagement.AccountRegistration
                 Dim objInterface As Object = Nothing
 
                 If ExternalInterface.Contains(",") Then
-                    Dim strAssembly As String = ExternalInterface.Split(Char.Parse(","))(0).Trim
-                    Dim strClass As String = ExternalInterface.Split(Char.Parse(","))(1).Trim
+                    Dim strClass As String = ExternalInterface.Split(Char.Parse(","))(0).Trim
+                    Dim strAssembly As String = ExternalInterface.Split(Char.Parse(","))(1).Trim
                     objInterface = System.Activator.CreateInstance(strAssembly, strClass).Unwrap
                 End If
 
